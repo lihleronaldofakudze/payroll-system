05a9ce6b9a9fd4297fa9aa7fc2b3cdcebe1d4a35
CodeCloutDev <71075659+CodeCloutDevs@users.noreply.github.com>
2022-04-07 06:52:16 +0200

    started intergrating database

--- src/Job.java
+++ src/Job.java
@@ -3,4 +3,11 @@ public class Job {
     String job_title;
     String job_type;
     String job_description;
+
+    public Job(int job_id, String job_title, String job_type, String job_description) {
+        this.job_id = job_id;
+        this.job_title = job_title;
+        this.job_type = job_type;
+        this.job_description = job_description;
+    }
 }
